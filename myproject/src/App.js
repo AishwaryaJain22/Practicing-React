f57f3eaa5fb1dff0fd6d1b55c9cf7870ac852cee
aishwarya <aishwaryajm321@gmail.com>
2022-12-11 21:33:45 +0530

    new changes

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,56 +1,75 @@
-import './App.css';
-import Count from './Count';
-import Profile from './Profile';
-import React, { Component } from 'react'
-import Lifecycle from './Lifecycle';
+import "./App.css";
+import Count from "./Count";
+import Profile from "./Profile";
+import React, { Component } from "react";
+import Lifecycle from "./Lifecycle";
+
+const employeeDrioctory = [
+  {
+    name: "Aishwarya",
+    position: "Frontend Developer",
+    id: "1",
+  },
+  {
+    name: "Utkarsh",
+    position: "Backend Developer",
+    id: "2",
+  },
+  {
+    name: "Shweta",
+    position: "QA Developer",
+    id: "3",
+  },
+];
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = ({
-      showLifeCycleComponent : true,
-      employee : [
-        {
-          name : 'Aishwarya',
-          position : 'Frontend Developer'
-        },
-        {
-          name : 'Utkarsh',
-          position : 'Backend Developer'
-        },
-        {
-          name : 'Shweta',
-          position : 'QA Developer'
-        },
-      ],
-      
-    })
-    
+    this.state = {
+      showLifeCycleComponent: true,
+      employee: employeeDrioctory,
+      employeOfTheMonth : '0'
+    }
   }
 
   hamdelToggel = () => {
     this.setState({
-      showLifeCycleComponent :  !this.state.showLifeCycleComponent
+      showLifeCycleComponent: !this.state.showLifeCycleComponent,
     });
   };
 
+  employeeOfTheMonth = employeeId =>{
+    this.setState({
+      employeOfTheMonth : employeeId
+    })
+  }
+
   render() {
     return (
-      <div className='App'>
-            <h1>My Application</h1>
-            <Count/>
-            {
-              this.state.employee.map(employe => {
-                return <Profile name={employe.name} position={employe.position}/>
-              })
-            }
-            <button onClick={this.hamdelToggel}>
-              {" "}
-               Click to show or hide thw lifecycle component
-               </button>
-            {this.state.showLifeCycleComponent ? <Lifecycle/> : <div> </div>}
+      <div className="App">
+        <h1>My Application</h1>
+        <Count />
+        {this.state.employee.map((employe) => {
+          //console.log(employe);
+          return (
+            <div style={{backgroundColor : employe.id === this.state.employeOfTheMonth ? "yellow": "white"}}>
+            <Profile
+              key={employe.id}
+              id={employe.id}
+              name={employe.name}
+              position={employe.position}
+              employeeOfTheMonth={this.employeeOfTheMonth}
+            />
+            </div>
+          );
+        })}
+        <button onClick={this.hamdelToggel}>
+          {" "}
+          Click to show or hide thw lifecycle component
+        </button>
+        {this.state.showLifeCycleComponent ? <Lifecycle /> : <div> </div>}
       </div>
-    )
+    );
   }
 }
 
--- myproject/src/Profile.js
+++ myproject/src/Profile.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
- class Profile extends Component {
+class Profile extends Component {
+  eventHandel = () => {
+    this.props.employeeOfTheMonth(this.props.id);
+  };
   render() {
     return (
       <div>
-        <img src='https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg' width='100px' height='100px'/>
+        <img
+          src="https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
+          width="100px"
+          height="100px"
+        />
         <h5>{this.props.name}</h5>
         <h6>{this.props.position}</h6>
+        <button onClick={this.eventHandel}>
+          Set {this.props.name} is the employee of the month
+        </button>
         <p>--------------------------</p>
       </div>
-    )
+    );
   }
 }
 export default Profile;
